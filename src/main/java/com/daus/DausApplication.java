65299f8f8691dcbc4d49d5971862aca4303a7c83
Jordi M. <jmiretm@gmail.com>
2020-06-17 09:57:04 +0200

    First commit

---
+++ src/main/java/com/daus/DausApplication.java
@@ -0,0 +1,19 @@
+package com.daus;
+
+/**
+ * 
+ * @author jordi.miret
+ * 
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DausApplication.class, args);
+	}
+
+}
